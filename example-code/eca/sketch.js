89058edfb08a1ebd30a46aa10bbf16ede3150728
mdarfler <mdarfler@gmail.com>
2019-02-18 22:04:28 -0500

    Add interaction

--- example-code/eca/sketch.js
+++ example-code/eca/sketch.js
@@ -1,10 +1,12 @@
 let CA = [];
 const screenWidth = 800;
 const screenHeight = 400;
-const numCells = 100;
-const cellSize = (screenWidth-1)/numCells
+const numCells = 300;
+const cellSize = (screenWidth - 1) / numCells
+let ruleNum, startStop, reset,pixelsize
+let running = 1;
 
-const ruleset = [0,1,0,1,1,0,1,0]
+let ruleset;
 
 function setup() {
     createCanvas(screenWidth, screenHeight);
@@ -12,53 +14,91 @@ function setup() {
     fill(0);
 
     let gen0 = new Array(numCells).fill(0);
-    gen0[floor(screenWidth/cellSize/2)] = 1
+    gen0[floor(screenWidth / cellSize / 2)] = 1
     CA.push(gen0)
+
+
+
+    startStop = createButton('start/stop')
+    startStop.mousePressed(() => {
+        running = (running + 1) % 2
+        console.log(running)
+    })
+
+    reset = createButton('reset')
+    reset.mousePressed(tidy)
+
+    ruleNum = createInput(90)
+    getRuleSet()
+    ruleNum.changed(getRuleSet)
 }
 
- function draw() {
-    display()
-    generate();
-    console.log(CA.length)
-    if (CA.length * cellSize > windowHeight) CA.shift()
+function draw() {
+    if (running) {
+        display()
+        generate();
+        if (CA.length * cellSize > windowHeight) CA.shift()
+    }
 }
 
-function display(){
+function display() {
     background(255);
 
-    for(let i = 0; i < CA.length; i++){
-        for(let j = 0; j < numCells; j++){
-            if(CA[i][j] == 1){
-                rect(j * cellSize, i* cellSize, cellSize, cellSize);
+    for (let i = 0; i < CA.length; i++) {
+        for (let j = 0; j < numCells; j++) {
+            if (CA[i][j] == 1) {
+                rect(j * cellSize, i * cellSize, cellSize, cellSize);
             }
         }
     }
 }
 
-function generate(){
+function generate() {
     const current = CA[CA.length - 1];
     let nextGen = new Array(numCells).fill(0);
 
-    for (let i = 1; i < numCells - 1; i ++){
-        nextGen[i] = applyRules(current[i-1], current[i], current[i+1])
+    for (let i = 1; i < numCells - 1; i++) {
+        nextGen[i] = applyRules(current[i - 1], current[i], current[i + 1])
     }
     CA.push(nextGen)
 
 }
 
-function applyRules(a,b,c){
+function applyRules(a, b, c) {
 
-    if     (a == 1 && b == 1 && c == 1) return ruleset[0]
-    else if(a == 1 && b == 1 && c == 0) return ruleset[1]
-    else if(a == 1 && b == 0 && c == 1) return ruleset[2]
-    else if(a == 1 && b == 0 && c == 0) return ruleset[3]
-    else if(a == 0 && b == 1 && c == 1) return ruleset[4]
-    else if(a == 0 && b == 1 && c == 0) return ruleset[5]
-    else if(a == 0 && b == 0 && c == 1) return ruleset[6]
-    else if(a == 0 && b == 0 && c == 0) return ruleset[7]
-    else return null 
+    if (a == 1 && b == 1 && c == 1) return ruleset[0]
+    else if (a == 1 && b == 1 && c == 0) return ruleset[1]
+    else if (a == 1 && b == 0 && c == 1) return ruleset[2]
+    else if (a == 1 && b == 0 && c == 0) return ruleset[3]
+    else if (a == 0 && b == 1 && c == 1) return ruleset[4]
+    else if (a == 0 && b == 1 && c == 0) return ruleset[5]
+    else if (a == 0 && b == 0 && c == 1) return ruleset[6]
+    else if (a == 0 && b == 0 && c == 0) return ruleset[7]
+    else return null
 }
 
-function trim(){
+function trim() {
     if (CA.length * cellSize > windowHeight) CA.shift()
+}
+
+function tidy(){
+    CA = [];
+    let gen0 = new Array(numCells).fill(0);
+    gen0[floor(screenWidth / cellSize / 2)] = 1
+    CA.push(gen0)
+}
+
+function getRuleSet(){ 
+    tidy();
+
+    const temp = parseInt(ruleNum.value()).toString(2)
+    ruleset = [];
+    for(let i = 0; i < temp.length; i++){
+        ruleset.push(parseInt(temp[i]))
+    }
+    while(ruleset.length < 8){
+        ruleset.unshift(0)
+    }
+    console.log(ruleset)
+
 }
